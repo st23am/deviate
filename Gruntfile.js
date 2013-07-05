d57b6636354b6e2153c6af6451d850f59e083cb3
James Smith <James@gaslight.co>
2013-07-05 02:02:26 -0400

    move css files around try and get Grunt build working

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,14 @@ module.exports = function (grunt) {
           imagesDir: 'app/images',
           javascriptsDir: '.tmp/scripts',
           force: true
-        }
+        },
+        files: {
+           '<%= yeoman.dist %>/styles/main.css': [
+             '.tmp/styles/{,*/}*.css',
+             '<%= yeoman.app %>/styles/{,*/}*.scss',
+             '<%= yeoman.app %>/styles/{,*/}*.sass'
+           ]
+       }
       }
     },
     coffee: {
@@ -197,14 +204,16 @@ module.exports = function (grunt) {
       // By default, your `index.html` <!-- Usemin Block --> will take care of
       // minification. This option is pre-configured if you do not wish to use
       // Usemin blocks.
-      // dist: {
-      //   files: {
-      //     '<%= yeoman.dist %>/styles/main.css': [
-      //       '.tmp/styles/{,*/}*.css',
-      //       '<%= yeoman.app %>/styles/{,*/}*.css'
-      //     ]
-      //   }
-      // }
+       dist: {
+         files: {
+           '<%= yeoman.dist %>/styles/main.css': [
+             '.tmp/styles/{,*/}*.css',
+             '<%= yeoman.app %>/styles/{,*/}*.css',
+             '<%= yeoman.app %>/styles/{,*/}*.scss',
+             '<%= yeoman.app %>/styles/{,*/}*.sass'
+           ]
+         }
+       }
     },
     htmlmin: {
       dist: {
